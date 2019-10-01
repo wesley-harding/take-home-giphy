cca9d169dfc72a12f4b2f994ead9fa7107829aaa
Wesley Harding <hardingwesley28@gmail.com>
2019-09-30 22:17:05 -0500

    Moving some stuff around.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./App";
 
 ReactDOM.render(
     <App />,
@@ -12,7 +12,7 @@ declare let module: { hot: any };
 
 if (module.hot) {
   module.hot.accept("./components/App", () => {
-    const NewApp = require("./components/App").default;
+    const NewApp = require("./App").default;
 
     ReactDOM.render(<NewApp />, document.getElementById("app"));
   });
